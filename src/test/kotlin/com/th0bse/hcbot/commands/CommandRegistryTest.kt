cc0ae08b23c3c60aa8946493140eddd55d01fde4
Thorben von Rekowski <rekowski.thorben@th0bse.com>
2021-04-20 00:35:27 +0200

    test: Add rudimentary first test
    This still is more of a little exercise to get familiar with JUnit
    again, the one single test for the CommandRegistry isn't really that
    impressive at all.
    
    Issues: HCB-12
    Author: Thorben von Rekowski <rekowski.thorben@th0bse.com>

---
+++ src/test/kotlin/com/th0bse/hcbot/commands/CommandRegistryTest.kt
@@ -0,0 +1,35 @@
+package com.th0bse.hcbot.commands
+
+import dev.kord.core.event.message.MessageCreateEvent
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+
+internal class CommandRegistryTest {
+
+    val commandRegistry = CommandRegistry()
+
+    @Test
+    fun registerCommand() {
+        commandRegistry.registerCommand(TestCommand)
+        assertEquals(commandRegistry.getCommand("testCommand"), TestCommand)
+    }
+
+    // Command object used for testing. We create this here and don't use an existing command like Ping, to have full
+    // control over its behavior.
+    object TestCommand: Command {
+        override val name: String = "testCommand"
+        override val description: String = "This is a simple command created for testing purposes."
+
+        override suspend fun execute(event: MessageCreateEvent) {
+            event.message.getChannel().createMessage("Success")
+        }
+
+        override suspend fun printDescription(event: MessageCreateEvent) {
+            event.message.getChannel().createMessage(description)
+        }
+
+    }
+}
