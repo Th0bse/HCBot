c55671433032801b368b30327c2e2342ddc54319
Thorben von Rekowski <rekowski.thorben@th0bse.com>
2021-05-15 14:30:23 +0200

    ci: Attemp fix for teamcity, add features section
    Since the commitStatusPublisher is describes as a "build feature" in the
    teamcity web interface, it may just have to be added to a "features"
    section in the settings.kts.
    
    Author: Thorben von Rekowski <rekowski.thorben@th0bse.com>

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -59,19 +59,24 @@ object Build : BuildType({
         vcs {
         }
     }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:80e454be-d677-473c-918b-3ba92e7b10bf"
+                }
+            }
+            param("github_oauth_user", "th0bse")
+        }
+    }
+
     check(allowExternalStatus == false) {
         "Unexpected option value: allowExternalStatus = $allowExternalStatus"
     }
     allowExternalStatus = true
 
-    commitStatusPublisher {
-        vcsRootExtId = "${DslContext.settingsRoot.id}"
-        publisher = github {
-            githubUrl = "https://api.github.com"
-            authType = personalToken {
-                token = "credentialsJSON:80e454be-d677-473c-918b-3ba92e7b10bf"
-            }
-        }
-        param("github_oauth_user", "th0bse")
-    }
+
 })
